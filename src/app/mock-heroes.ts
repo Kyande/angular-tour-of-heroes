9cdcc6b4908f157aec7d2c804f22cb55cbd4b89e
kyande <kyande20@gmail.com>
2018-07-31 15:50:06 +0300

    angular-tour-of-heroes-tut

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Kyande'},
+    {id: 12, name: 'Michael'},
+    {id: 13, name: 'John'},
+    {id: 14, name: 'Jane'},
+    {id: 15, name: 'windstorm'},
+    {id: 16, name: 'Magma'},
+    {id: 17, name: 'Tornado'},
+    {id: 18, name: 'Narco'},
+    {id: 19, name: 'Bombastic'},
+    {id: 20, name: 'Fantastic'}
+];
